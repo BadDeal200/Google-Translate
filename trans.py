85a7952ca19952bbd68ed78c679dcbf20090e89c
BadDeal200 <92075792+BadDeal200@users.noreply.github.com>
2021-10-08 10:36:09 +0530

    Add files via upload

---
+++ trans.py
@@ -0,0 +1,10 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+print( f'{Back.CYAN}{Fore.RED} _____                   _       _         ')
+print( f'{Back.CYAN}{Fore.RED}|_   _|__ __ _ _ __  ___| | __ _| |_ __ _  ')
+print( f'{Back.CYAN}{Fore.RED}  | ||  __/ _  |  _ \/ __| |/ _  | __/ _ \ ')
+print( f'{Back.CYAN}{Fore.RED}  | || | | (_| | | | \__ \ | (_| | ||  __/ ')
+print( f'{Back.CYAN}{Fore.RED}  |_||_|  \__,_|_| |_|___/_|\__,_|\__\___| ')
+print()
