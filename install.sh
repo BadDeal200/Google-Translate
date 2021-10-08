555de0067c554a915a128fda49d3f1f362076026
BadDeal200 <92075792+BadDeal200@users.noreply.github.com>
2021-10-08 10:31:48 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+figlet Installing | lolcat
+echo -e "\033[31m" 
+pkg install translate-shell | lolcat
+clear
+figlet Installing | lolcat
+echo -e "\033[31m"
+pkg install pulseaudio | lolcat
+clear
+figlet Installing | lolcat
+echo -e "\033[31m"
+pkg install sox | lolcat
+clear
+figlet Done | lolcat
